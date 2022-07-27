2fff8f00262e10c1ba030deb799262866e74a53a
priscila-espinoza333 <pr.espinoza@duocuc.cl>
2022-07-27 02:33:18 -0400

    preguntas js

---
+++ Espinoza_Priscila_JSPredicción.js
@@ -0,0 +1,21 @@
+// Pregunta 1 
+function myBirthYearFunc(){
+    console.log("Nací en " + 1980);
+}
+    //Respuesta: console.log indicará "Nací en 1980"
+
+// Pregunta 2
+function myBirthYearFunc(EntradaAñoNacimiento){
+    console.log("Nací en " + EntradaAñoNacimiento);
+}
+    // Respuesta: console.log inicará "Naci en 1980"
+
+//Pregunta 3
+function add(num1, num2){
+    console.log("¡Sumando números!");
+    console.log("num1 is: " + num1);
+    console.log("num2 is: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+}
+    // Respuesta: console.log indicará "30"    
